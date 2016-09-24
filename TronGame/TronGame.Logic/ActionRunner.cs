994c07f8b04db7ab3982174b8dd90c799e913b06
Edwin Herrera <edwherrera.175@gmail.com>
2016-09-24 15:34:37 -0600

    ActionRunner Singleton added

---
+++ TronGame/TronGame.Logic/ActionRunner.cs
@@ -0,0 +1,25 @@
+﻿namespace TronGame.Logic
+{
+    public class ActionRunner
+    {
+        private static ActionRunner _instance;
+
+        protected ActionRunner()
+        {
+
+        }
+
+        public ActionRunner Instance()
+        {
+            return _instance ?? (_instance = new ActionRunner());
+        }
+
+        public Space PerformAction(ICommand command, int maxWidth, int maxHeight)
+        {
+            command.Run();
+            command.Player.Position.XPos %= maxWidth;
+            command.Player.Position.YPos %= maxHeight;
+            return command.Player.Position;
+        }
+    }
+}
