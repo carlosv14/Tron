0af87d6a868c9dd899670e762975c02ea14a6e95
Edwin Herrera <edwherrera.175@gmail.com>
2016-09-24 09:21:52 -0600

    Spaces added and also player has move method

--- TronGame/TronGame.Logic/Player.cs
+++ TronGame/TronGame.Logic/Player.cs
@@ -6,5 +6,13 @@ namespace TronGame.Logic
     {
         public string Name { get; set; }
         public Color Color{ get; set; }
+        public Space Position { get; set; }
+
+        public Player Move(int vertical, int horizontal)
+        {
+            Position.XPos += horizontal;
+            Position.YPos += vertical;
+            return this;
+        }
     }
 }
---
+++ TronGame/TronGame.Logic/Space.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace TronGame.Logic
+{
+    public class Space : IEqualityComparer<Space>
+    {
+        public int XPos { get; set; }
+        public int YPos { get; set; }
+
+        public Space(int xPos, int yPos)
+        {
+            XPos = xPos;
+            YPos = yPos;
+        }
+
+        public bool Equals(Space x, Space y)
+        {
+            return x.XPos == y.XPos && y.XPos == x.YPos;
+        }
+
+        public int GetHashCode(Space space)
+        {
+            return XPos.GetHashCode() ^ YPos.GetHashCode();
+        }
+    }
+}
